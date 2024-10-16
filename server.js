705aa624b57dab2624cb23f86ea311d84f1eaa56
srivardhan karanam <karanam.sr@northeastern.edu>
2024-10-15 23:38:15 -0400

    dcd

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.disable('x-powered-by');
 
 
 
+
+
 const sequelize = new Sequelize(process.env.DB_name, process.env.DB_username, process.env.DB_password, {
   host: process.env.DB_host,
   dialect: 'mysql',
